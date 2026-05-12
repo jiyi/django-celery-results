463068f796411118da2986be11ab5b3f1e68aa54
Jiyi <jiyilc.sd@chinatelecom.cn>
2026-05-12 19:22:11 +0800

    Add display methods to decode unicode escapes in admin

--- django_celery_results/admin.py
+++ django_celery_results/admin.py
@@ -1,8 +1,22 @@
 """Result Task Admin interface."""
 
+import re
+
 from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+
+
+def _decode_unicode_escapes(value):
+    """Convert \\uXXXX escape sequences to actual Unicode characters for display."""
+    if not isinstance(value, str):
+        return value
+
+    def replace_unescape(m):
+        return chr(int(m.group(1), 16))
+
+    return re.sub(r'\\u([0-9a-fA-F]{4})', replace_unescape, value)
+
 
 try:
     ALLOW_EDITS = settings.DJANGO_CELERY_RESULTS['ALLOW_EDITS']
@@ -23,7 +37,7 @@ class TaskResultAdmin(admin.ModelAdmin):
     list_filter = ('status', 'date_done', 'periodic_task_name', 'task_name',
                    'worker')
     readonly_fields = ('date_created', 'date_started', 'date_done',
-                       'result', 'meta')
+                       'display_result', 'meta')
     search_fields = ('task_name', 'task_id', 'status', 'task_args',
                      'task_kwargs')
     fieldsets = (
@@ -41,14 +55,14 @@ class TaskResultAdmin(admin.ModelAdmin):
         }),
         (_('Parameters'), {
             'fields': (
-                'task_args',
-                'task_kwargs',
+                'display_task_args',
+                'display_task_kwargs',
             ),
             'classes': ('extrapretty', 'wide')
         }),
         (_('Result'), {
             'fields': (
-                'result',
+                'display_result',
                 'date_created',
                 'date_started',
                 'date_done',
@@ -59,13 +73,25 @@ class TaskResultAdmin(admin.ModelAdmin):
         }),
     )
 
+    def display_task_args(self, obj):
+        return _decode_unicode_escapes(obj.task_args)
+    display_task_args.short_description = _('Task Positional Arguments')
+
+    def display_task_kwargs(self, obj):
+        return _decode_unicode_escapes(obj.task_kwargs)
+    display_task_kwargs.short_description = _('Task Named Arguments')
+
+    def display_result(self, obj):
+        return _decode_unicode_escapes(obj.result)
+    display_result.short_description = _('Result Data')
+
     def get_readonly_fields(self, request, obj=None):
         if ALLOW_EDITS:
             return self.readonly_fields
         else:
             return list({
                 field.name for field in self.model._meta.fields
-            })
+            } | {'display_task_args', 'display_task_kwargs', 'display_result'})
 
 
 admin.site.register(TaskResult, TaskResultAdmin)
